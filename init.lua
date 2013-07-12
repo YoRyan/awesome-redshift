fdb67cf51922d90341af055d0b4c5d11a7fe746c
Ryan Young <ryan@ryannet.strangled.net>
2013-07-11 22:49:40 -0700

    Add support for different temperature-setting methods

--- init.lua
+++ init.lua
@@ -10,31 +10,44 @@ local awful = require("awful")
 -- variables
 local redshift = {}
 redshift.redshift = "/usr/bin/redshift"    -- binary path
+redshift.method = "randr"                  -- randr or vidmode
 redshift.options = ""                      -- additional redshift command options
 redshift.state = 1                         -- 1 for screen dimming, 0 for none
 redshift.timer = timer({ timeout = 60 })
 
 -- functions
 redshift.dim = function()
-    local screens = screen.count()
-    for i = 0, screens - 1
-    do
-        awful.util.spawn(redshift.redshift .. " -m vidmode:screen=" .. i .. " -o "
-                             .. redshift.options)
+    if redshift.method == "randr"
+    then
+        awful.util.spawn(redshift.redshift .. " -m randr -o " .. redshift.options)
+    elseif redshift.method == "vidmode"
+    then
+        local screens = screen.count()
+        for i = 0, screens - 1
+        do
+            awful.util.spawn(redshift.redshift .. " -m vidmode:screen=" .. i ..
+                             " -o " .. redshift.options)
+        end
     end
     redshift.state = 1
     redshift.timer:start()
 end
 redshift.timer:connect_signal("timeout", redshift.dim)
 redshift.undim = function()
-    local screens = screen.count()
-    for i = 0, screens - 1
-    do
-        awful.util.spawn(redshift.redshift .. " -m vidmode:screen=" .. i .. " -x "
-                             .. redshift.options)
+    if redshift.method == "randr"
+    then
+        awful.util.spawn(redshift.redshift .. " -m randr -x " .. redshift.options)
+    elseif redshift.method == "vidmode"
+    then
+        local screens = screen.count()
+        for i = 0, screens - 1
+        do
+            awful.util.spawn(redshift.redshift .. " -m vidmode:screen=" .. i ..
+                             " -x " .. redshift.options)
+        end
     end
     redshift.state = 0
-    redshift.timer:stop()
+    redshift.timer:start()
 end
 redshift.toggle = function()
     if redshift.state == 1
